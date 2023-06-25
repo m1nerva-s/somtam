a7b3bbd332a69390645d819eb1cba36eec770f36
m1nerva-s <85502719+m1nerva-s@users.noreply.github.com>
2023-06-25 10:49:00 +0700

    cook somtam sud zaap🌶️

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { somtamList } from "../somtamList";
+  // import { Card } from "../components/Card.svelte";
+</script>
+
+<div class="w-screen min-h-screen bg-black align-middle">
+  <div class="w-screen bg-black p-6 pl-8 md:pl-16 border-b-4 border-primary mb-16">
+    <img src="icon_somtam.png" class="w-32" alt="logo" />
+  </div>
+  <div class="container mx-auto">
+    <div class="flex flex-wrap justify-center">
+      {#each somtamList as somtam}
+        <div class="px-4 mb-8">
+          <div class=" px-4 mb-8">
+            <div class="relative w-full h-fit">
+              <img style="max-width:300px;" class="aspect-video object-fill" src={somtam.img} alt={somtam.somtamName}>
+            </div>
+            <div class="mt-2">
+              <h2 class="text-xl font-semibold text-white">{somtam.somtamName}</h2>
+            </div>
+          </div>
+          </div>
+      {/each}
+    </div>
+  </div>
+</div>
---
+++ src/somtamList.ts
@@ -0,0 +1,15 @@
+export const somtamList = [
+  { somtamName: "Som Tam Poo Para", img: "somtam1.jpg" },
+  { somtamName: "Som Tam Esan", img: "somtam2.jpg" },
+  { somtamName: "Som Tam Thai", img: "somtam3.jpg" },
+  { somtamName: "Som Tam Thai", img: "somtam4.jpg" },
+  { somtamName: "Som Tam Pla Too", img: "somtam5.jpg" },
+  { somtamName: "Som Tam Sud Sap", img: "somtam6.jpg" },
+  { somtamName: "Som Tam Aroi Hor", img: "somtam7.jpg" },
+  { somtamName: "Som Tam Homemade", img: "somtam8.jpg" },
+  { somtamName: "Som Tam Tam Sod Sod", img: "somtam9.jpg" },
+  { somtamName: "Som Tam Na ", img: "somtam10.jpg" },
+  { somtamName: "Som Tam Expensive", img: "somtam11.jpg" },
+  { somtamName: "Happy Elder like Som Tam", img: "happy_elder.jpg" },
+]
+export type ISomtam = typeof somtamList[number]
